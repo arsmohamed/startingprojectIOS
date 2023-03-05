d6fad850fcf7f831a9f156b708cde233f7a4c5e0
Amr Mohamed <mero@Amrs-MacBook-Air.local>
2023-03-05 14:20:56 -0330

    I am done with planning the views and navigation task and I will start with the last task to submit

--- StartingProject/LocationsView.swift
+++ StartingProject/LocationsView.swift
@@ -9,15 +9,20 @@ struct LocationsView: View {
             LittleLemonLogo() // 1
                 .padding(.top, 50)
             
-            Text(myReservationValue.displayingReservationForm ? "Reservation Details" : "Select a location") // 2
+            Text(myReservationValue.displayingReservationForm ?
+                 "Reservation Details" : "Select a location") // 2
             .padding([.leading, .trailing], 40)
             .padding([.top, .bottom], 8)
             .background(Color.gray.opacity(0.2))
             .cornerRadius(20)
             
             NavigationView {
-                List {
-                    ReservationView()
+                NavigationLink(destination: ReservationForm(Reservation().restaurant)){
+                    List {
+                        ForEach(myReservationValue.restaurants, id: \.self ) { e in
+                            RestaurantView(e)
+                        }
+                    }
                 }
             }// 3
             .navigationBarTitle("")
